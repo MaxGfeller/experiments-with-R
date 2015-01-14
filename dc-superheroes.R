e8994a627aa706d5fe4f49644795b18a0fa04e7c
Max Gfeller <mg@cyon.ch>
2015-01-14 07:39:31 +0100

    Document code

--- dc-superheroes.R
+++ dc-superheroes.R
@@ -1,7 +1,14 @@
+# read data from csf file
 data <- read.csv("test-data/comic-characters/dc-wikia-data.csv", TRUE)
+
+# redirect plot output into file
 jpeg("output/number-of-heroes.jpg")
 hist(data$YEAR, main="Number of new superheroes", xlab="year", ylab="Number of new superheroes")
+
+# create subset for female characters
 femaleCharacters <- subset(data, SEX=='Female Characters')
 jpeg("output/number-of-female-characters.jpg")
 hist(femaleCharacters$YEAR, main="Number of female characters", xlab="Year", ylab="Number of female characters")
+
+# sign off
 dev.off()
