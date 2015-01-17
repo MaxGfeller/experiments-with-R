77e319bd79645232a6be5a3398edff3a09306866
Max Gfeller <max.gfeller@gmail.com>
2015-01-17 19:33:40 +0100

    Move images into sub directory

--- dc-superheroes.R
+++ dc-superheroes.R
@@ -2,12 +2,12 @@
 data <- read.csv("test-data/comic-characters/dc-wikia-data.csv", TRUE)
 
 # redirect plot output into file
-jpeg("output/number-of-heroes.jpg")
+jpeg("output/dc-superheroes/number-of-heroes.jpg")
 hist(data$YEAR, main="Number of new superheroes", xlab="year", ylab="Number of new superheroes")
 
 # create subset for female characters
 femaleCharacters <- subset(data, SEX=='Female Characters')
-jpeg("output/number-of-female-characters.jpg")
+jpeg("output/dc-superheroes/number-of-female-characters.jpg")
 hist(femaleCharacters$YEAR, main="Number of female characters", xlab="Year", ylab="Number of female characters")
 
 # sign off
